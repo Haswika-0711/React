93a471860f80fb1f11c7ff12039d429f0a1d7afa
Haswika-0711 <user278@ALCHEMYCLOUD.CO.IN>
2020-10-27 17:31:35 +0530

    basics of react n jsx

---
+++ Day-4/REACTMYAPPLICATION/App.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+
+class App extends React.Component{
+    render(){
+        return(
+            <div> 
+               <h1>Shopping list</h1>
+               <ul>
+                   <li>Facebook</li>
+                   <li>Insta</li>
+                   <li>youtube</li>
+               </ul>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+// import App2 from './App2.jsx'
+// import React from "react"
+// class App extends React.Component{
+//     render(){
+//         return(
+//             <div> 
+//                <table border="3px">
+//                    <tr>
+//                        <td>101</td>
+//                        <td><App2 /></td>
+//                    </tr>
+//                </table>
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
---
+++ Day-4/REACTMYAPPLICATION/App2.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+class App2 extends React.Component{
+    render(){
+        return(
+            <div> 
+               welcome
+            </div>
+        );
+    }
+}
+
+export default App2;
